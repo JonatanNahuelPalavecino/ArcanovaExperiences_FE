30347b76062566589345acc6668eee22889075f2
Jonatan Palavecino <jnpr1992@hotmail.com>
2025-07-01 20:54:26 -0300

    Se creo la base de la seccion about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { BrowserRouter } from "react-router-dom"
-import { NavBar } from "./components/NavBar/NavBar"
-import { Slider } from "./components/Slider/Slider"
-import "./App.scss"
+import { BrowserRouter } from "react-router-dom";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Slider } from "./components/Slider/Slider";
+import "./App.scss";
+import { AboutUs } from "./components/AboutUs/AboutUs";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-        <NavBar/>
-        <Slider/>
+        <NavBar />
+        <Slider />
+        <AboutUs/>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+//ARRANCAS A CREAR ABOUT US. VERIFICA LOGICA PARA QUE LA NAVEGACION SEA POR SCROLL. BASATE EL LA PAGINA QUE TE PASO. PEDI A CHATGPT UN ABOuT US SEGUN EL SERVICIO QUE OFRECE y EN INGLES; con TITULO y UNA BREVE DESCRIPCION
